2db18354c5c513cf29a60a22857b3acbaf119e65
gilpop8663 <wolfye@naver.com>
2022-04-15 13:09:11 +0900

    Add: 이벤트 버블링,이벤트  캡쳐 글 추가 , 패터디 로고 추가

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ function HomePage() {
 
       <div className="container max-w-4xl m-auto px-4 mt-20">
         <Image
-          src="/desk.jpg"
-          alt="my desk"
-          width={1920 / 2}
-          height={1280 / 2}
+          src="/images/fetudyLogo.png"
+          alt="FETUDY LOGO"
+          width={1500 / 2}
+          height={1500 / 2}
         />
       </div>
     </>
